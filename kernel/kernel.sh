441101acf8c9ae17a29924b9dc1e8b219a0edb87
ook37 <oren+12345@taumoda.com>
2024-03-18 18:59:10 -0400

    update kernel.sh to be more auto

--- kernel/kernel.sh
+++ kernel/kernel.sh
@@ -11,6 +11,11 @@ if [[ -z $NO_COLOR ]]; then
   export BWhite=$'\033[1;37m'
 fi
 
+if [[ ! -d .git || $(basename `git rev-parse --show-toplevel`) != "pacstall-programs" ]]; then
+  echo "${BRed}ERROR${NC}: not in a ${BCyan}pacstall-programs${NC} git clone."
+  exit 1
+fi
+
 function ask() {
     local prompt default reply
 
@@ -72,21 +77,19 @@ function ask() {
     done
 }
 
-ask "Are you updating stable kernel packages?" N
-if ((answer == 0)); then
-    stable="N"
-else
-    stable="Y"
-fi
+function fetch_kver() {
+  local keroutput
+  keroutput=$(curl -fsSL "https://kernel.ubuntu.com/mainline/v${kerver}/amd64/CHECKSUMS" 2> /dev/null | grep 'Sha256' -A 4 | grep -v 'Sha256' && \
+  curl -fsSL "https://kernel.ubuntu.com/mainline/v${kerver}/arm64/CHECKSUMS" 2> /dev/null | grep 'Sha256' -A 6 | grep -v 'Sha256' | grep -v '64k')
+  mapfile -t input_data <<< "${keroutput}"
+  input_data+=("EOF")
+  if [[ -z ${input_data[7]} ]]; then
+    echo "${BRed}ERROR${NC}: incomplete ${BCyan}version${NC} response from server."
+    exit 1
+  fi
+}
 
-echo "Paste file inputs (${BPurple}type EOF when done${NC}):"
-input_data=()
-while IFS= read -r line; do
-  [[ $line == "EOF" ]] && break
-  input_data+=("$line")
-done
-
-parse_filename() {
+function parse_filename() {
   local filename=$1
   local name
 
@@ -99,128 +102,251 @@ parse_filename() {
     name="linux-image-unsigned"
   fi
 
-  [[ $stable == "y" || $stable == "Y" ]] && name+="-stable"
-  name+="-deb"
+  if $stable; then
+    name+="-stable"
+  fi
 
-  echo $name
+  echo "${name}-deb"
 }
 
-for line in "${input_data[@]}"; do
-  IFS=' ' read -r -a arr <<<"$line"
-  hash=${arr[0]}
-  package=${arr[1]}
-  if [[ $stable == "y" || $stable == "Y" ]]; then
-    buildver=$(echo "$package" | perl -nle'print $& if m{([0-9]+\.[0-9]+(-rc[0-9]+)?\.[0-9]+-[0-9]+\.?[0-9]*\.?[0-9]*\.[0-9]*)}')
-  else
+function update_ker() {
+  if [[ -z ${stable} ]]; then
+    ask "Are you updating stable kernel packages?" N
+    if ((answer == 0)); then
+        stable=false
+    else
+        stable=true
+    fi
+  fi
+
+  if [[ -z ${input_data[*]} ]]; then
+    echo "Paste file inputs (${BPurple}type EOF when done${NC}):"
+    input_data=()
+    while IFS= read -r line; do
+      [[ $line == "EOF" ]] && break
+      input_data+=("$line")
+    done
+  fi
+
+  for line in "${input_data[@]}"; do
+    IFS=' ' read -r -a arr <<<"$line"
+    [[ ${arr[0]} == "EOF" ]] && break
+    hash=${arr[0]}
+    package=${arr[1]}
+
     if [[ $package == *"rc"* ]]; then
+      if ${stable}; then
+        echo "${BYellow}WARNING${NC}: ${BPurple}stable${NC} cannot be an ${BCyan}rc${NC} release, switching to ${BPurple}mainline${NC}."
+        stable=false
+      fi
       buildver=$(echo "$package" | perl -nle'print $& if m{([0-9]+\.[0-9]+\.?[0-9]*-?[0-9]*rc?[0-9]*\.[0-9]+)}')
     else
       buildver=$(echo "$package" | perl -nle'print $& if m{([0-9]+\.[0-9]+(-rc[0-9]+)?\.[0-9]+-[0-9]+\.?[0-9]*\.?[0-9]*\.[0-9]*)}')
     fi
-  fi
-  gives=$(echo "$package" | perl -nle'print $& if m{(linux-.+?)_}')
-  gives=${gives%_} # Remove trailing underscore
-  if [[ $stable == "y" || $stable == "Y" ]]; then
-    pkgver=$(echo "$package" | perl -nle'print $& if m{([0-9]+\.[0-9]+\.[0-9]+)}')
-    pkgver=${pkgver//.0/} # Remove '.0' from pkgver
-  else
-    pkgver=$(echo "$gives" | perl -nle'print $& if m{([0-9]+\.[0-9]+)}') # Get the first set of numbers
-    rc_number="$(echo "$package" | perl -nle'print $& if m{(rc[0-9]+)}')" # Get the 'rc' string
-    if ! [[ "${rc_number}" == "" ]]; then
-      rc_number="~${rc_number}"
-    fi
-    pkgver+="${rc_number}" # Append "-rc#" to pkgver
-    pkgver=${pkgver//.0/} # Remove '.0' from pkgver
-  fi
-  pacscript=$(parse_filename "$package")
-
-  old_pkgver=$(perl -nle'print $& if m{(?<=pkgver=").*?(?=")}' "packages/$pacscript/$pacscript.pacscript")
-  old_buildver=$(perl -nle'print $& if m{(?<=buildver=").*?(?=")}' "packages/$pacscript/$pacscript.pacscript")
-  old_gives=$(perl -nle'print $& if m{(?<=gives=").*?(?=")}' "packages/$pacscript/$pacscript.pacscript")
-
-  if [[ -z $original_old_pkgver ]]; then
-    original_old_pkgver=$old_pkgver
-  fi
-
-  echo "For ${BPurple}$pacscript${NC},"
-  if [[ "$old_pkgver" == "$pkgver" ]]; then
-    echo "no pkgver change: ${BYellow}$pkgver${NC}"
-  else
-    echo "old pkgver: ${BRed}$old_pkgver${NC}"
-    echo "new pkgver: ${BGreen}$pkgver${NC}"
-  fi
-  if [[ "$old_buildver" == "$buildver" ]]; then
-    echo "no build pkgver change: ${BYellow}$buildver${NC}"
-  else
-    echo "old build pkgver: ${BRed}$old_buildver${NC}"
-    echo "new build pkgver: ${BGreen}$buildver${NC}"
-  fi
-  if [[ "$old_gives" == "$gives" ]]; then
-    echo "no gives change: ${BYellow}$gives${NC}"
-  else
-    echo "old gives: ${BRed}$old_gives${NC}"
-    echo "new gives: ${BGreen}$gives${NC}"
-  fi
-
-  hash_lines=$(grep -o 'hash=".*"' "packages/$pacscript/$pacscript.pacscript" | wc -l)
-  if ((hash_lines > 1)); then
-    if [[ $package == *"arm64"* ]]; then
-      old_hash=$(perl -nle'print $& if m{(?<=hash=").*?(?=")}' "packages/$pacscript/$pacscript.pacscript" | head -1)
-      if [[ "$old_hash" == "$hash" ]]; then
-        echo "no arm64 hash change: ${BYellow}$hash${NC}"
-      else
-        echo "old arm64 hash: ${BRed}$old_hash${NC}"
-        echo "new arm64 hash: ${BGreen}$hash${NC}"
+    gives=$(echo "$package" | perl -nle'print $& if m{(linux-.+?)_}')
+    gives=${gives%_}
+    if [[ $package == *"rc"* ]]; then
+      pkgver=$(echo "$gives" | perl -nle'print $& if m{([0-9]+\.[0-9]+)}') # Get the first set of numbers
+      rc_number="$(echo "$package" | perl -nle'print $& if m{(rc[0-9]+)}')" # Get the 'rc' string
+      if [[ -n ${rc_number} ]]; then
+        pkgver+="~${rc_number}" # Append "~rc#" to pkgver
       fi
-    elif [[ $package == *"amd64"* ]]; then
-      old_hash=$(perl -nle'print $& if m{(?<=hash=").*?(?=")}' "packages/$pacscript/$pacscript.pacscript" | tail -1)
-      if [[ "$old_hash" == "$hash" ]]; then
-        echo "no amd64 hash change: ${BYellow}$hash${NC}"
-      else
-        echo "old amd64 hash: ${BRed}$old_hash${NC}"
-        echo "new amd64 hash: ${BGreen}$hash${NC}"
+      if [[ $pkgver == *".0" ]]; then
+        pkgver=${pkgver%.0}
       fi
-    fi
-  else
-    old_hash=$(perl -nle'print $& if m{(?<=hash=").*?(?=")}' "packages/$pacscript/$pacscript.pacscript")
-    if [[ "$old_hash" == "$hash" ]]; then
-      echo "no hash change: ${BYellow}$hash${NC}"
     else
-      echo "old hash: ${BRed}$old_hash${NC}"
-      echo "new hash: ${BGreen}$hash${NC}"
+      pkgver=$(echo "$package" | perl -nle'print $& if m{([0-9]+\.[0-9]+\.[0-9]+)}')
+      if [[ $pkgver == *".0" ]]; then
+        pkgver=${pkgver%.0}
+      fi
     fi
-  fi
+    if ! $stable; then
+      if [[ $pkgver =~ ^[0-9]+\.[0-9]+\.[1-9][0-9]*$ ]]; then
+        echo "${BYellow}WARNING${NC}: ${BPurple}mainline${NC} cannot be an ${BWhite}X.X.${BRed}X${NC} release ending with ${BCyan}1-9${NC}, switching to ${BPurple}stable${NC}."
+        stable=true
+      fi
+    fi
 
-  echo -n "Are you sure you want to proceed with these changes? [${BGreen}y${NC}/${BRed}N${NC}] "
+    if ${newbranch} && [[ ${line} == "${input_data[0]}" ]]; then
+      case ${kerselected} in
+        both) gitker_branch="${pkgver}a" ;;
+        stab) gitker_branch="${pkgver}s" ;;
+        main) gitker_branch="${pkgver}" ;;
+      esac
+      if git branch | grep "${gitker_branch}" >> /dev/null && [[ $(git rev-parse --abbrev-ref HEAD) != "${gitker_branch}" ]]; then
+        git branch -D ${gitker_branch/\~/}
+      fi
+      git checkout -b ${gitker_branch/\~/}
+    fi
 
-  read -r response
-  if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
-    sed -i "" "s/pkgver=\".*\"/pkgver=\"$pkgver\"/" "packages/$pacscript/$pacscript.pacscript"
-    sed -i "" "s/buildver=\".*\"/buildver=\"$buildver\"/" "packages/$pacscript/$pacscript.pacscript"
-    sed -i "" "s/gives=\".*\"/gives=\"$gives\"/" "packages/$pacscript/$pacscript.pacscript"
+    pacscript=$(parse_filename "$package")
+
+    old_pkgver=$(perl -nle'print $& if m{(?<=pkgver=").*?(?=")}' "packages/$pacscript/$pacscript.pacscript")
+    old_buildver=$(perl -nle'print $& if m{(?<=buildver=").*?(?=")}' "packages/$pacscript/$pacscript.pacscript")
+    old_gives=$(perl -nle'print $& if m{(?<=gives=").*?(?=")}' "packages/$pacscript/$pacscript.pacscript")
+
+    if [[ -z $original_old_pkgver ]]; then
+      original_old_pkgver=$old_pkgver
+    fi
+
+    echo "For ${BPurple}$pacscript${NC},"
+    if [[ "$old_pkgver" == "$pkgver" ]]; then
+      echo "no pkgver change: ${BYellow}$pkgver${NC}"
+    else
+      echo "old pkgver: ${BRed}$old_pkgver${NC}"
+      echo "new pkgver: ${BGreen}$pkgver${NC}"
+    fi
+    if [[ "$old_buildver" == "$buildver" ]]; then
+      echo "no build pkgver change: ${BYellow}$buildver${NC}"
+    else
+      echo "old build pkgver: ${BRed}$old_buildver${NC}"
+      echo "new build pkgver: ${BGreen}$buildver${NC}"
+    fi
+    if [[ "$old_gives" == "$gives" ]]; then
+      echo "no gives change: ${BYellow}$gives${NC}"
+    else
+      echo "old gives: ${BRed}$old_gives${NC}"
+      echo "new gives: ${BGreen}$gives${NC}"
+    fi
+
+    hash_lines=$(grep -o 'hash=".*"' "packages/$pacscript/$pacscript.pacscript" | wc -l)
     if ((hash_lines > 1)); then
       if [[ $package == *"arm64"* ]]; then
-        awk -v hash="$hash" '/if \[\[ \${CARCH} == "arm64" \]\]; then/{c=1}
-                    c&&/hash="/{sub(/hash=".*"/, "hash=\""hash"\""); c=0} 1' "packages/$pacscript/$pacscript.pacscript" >tmpfile && mv tmpfile "packages/$pacscript/$pacscript.pacscript"
+        old_hash=$(perl -nle'print $& if m{(?<=hash=").*?(?=")}' "packages/$pacscript/$pacscript.pacscript" | head -1)
+        if [[ "$old_hash" == "$hash" ]]; then
+          echo "no arm64 hash change: ${BYellow}$hash${NC}"
+        else
+          echo "old arm64 hash: ${BRed}$old_hash${NC}"
+          echo "new arm64 hash: ${BGreen}$hash${NC}"
+        fi
       elif [[ $package == *"amd64"* ]]; then
-        awk -v hash="$hash" '/else/{c=1}
-                    c&&/hash="/{sub(/hash=".*"/, "hash=\""hash"\""); c=0} 1' "packages/$pacscript/$pacscript.pacscript" >tmpfile && mv tmpfile "packages/$pacscript/$pacscript.pacscript"
+        old_hash=$(perl -nle'print $& if m{(?<=hash=").*?(?=")}' "packages/$pacscript/$pacscript.pacscript" | tail -1)
+        if [[ "$old_hash" == "$hash" ]]; then
+          echo "no amd64 hash change: ${BYellow}$hash${NC}"
+        else
+          echo "old amd64 hash: ${BRed}$old_hash${NC}"
+          echo "new amd64 hash: ${BGreen}$hash${NC}"
+        fi
       fi
     else
-      sed -i "" "s/hash=\".*\"/hash=\"$hash\"/" "packages/$pacscript/$pacscript.pacscript"
+      old_hash=$(perl -nle'print $& if m{(?<=hash=").*?(?=")}' "packages/$pacscript/$pacscript.pacscript")
+      if [[ "$old_hash" == "$hash" ]]; then
+        echo "no hash change: ${BYellow}$hash${NC}"
+      else
+        echo "old hash: ${BRed}$old_hash${NC}"
+        echo "new hash: ${BGreen}$hash${NC}"
+      fi
     fi
+
+    if $promptless; then
+      answer="1"
+    else
+      ask "Are you sure you want to proceed with these changes?" N
+    fi
+    if ((answer == 1)); then
+      sed -i "" "s/pkgver=\".*\"/pkgver=\"$pkgver\"/" "packages/$pacscript/$pacscript.pacscript"
+      sed -i "" "s/buildver=\".*\"/buildver=\"$buildver\"/" "packages/$pacscript/$pacscript.pacscript"
+      sed -i "" "s/gives=\".*\"/gives=\"$gives\"/" "packages/$pacscript/$pacscript.pacscript"
+      if ((hash_lines > 1)); then
+        if [[ $package == *"arm64"* ]]; then
+          awk -v hash="$hash" '/if \[\[ \${CARCH} == "arm64" \]\]; then/{c=1}
+                      c&&/hash="/{sub(/hash=".*"/, "hash=\""hash"\""); c=0} 1' "packages/$pacscript/$pacscript.pacscript" >tmpfile && mv tmpfile "packages/$pacscript/$pacscript.pacscript"
+        elif [[ $package == *"amd64"* ]]; then
+          awk -v hash="$hash" '/else/{c=1}
+                      c&&/hash="/{sub(/hash=".*"/, "hash=\""hash"\""); c=0} 1' "packages/$pacscript/$pacscript.pacscript" >tmpfile && mv tmpfile "packages/$pacscript/$pacscript.pacscript"
+        fi
+      else
+        sed -i "" "s/hash=\".*\"/hash=\"$hash\"/" "packages/$pacscript/$pacscript.pacscript"
+      fi
+    else
+      exit 1
+    fi
+  done
+
+  kernel_script="packages/linux-kernel/linux-kernel.pacscript"
+  if $stable; then
+    kernel_script="packages/linux-kernel-stable/linux-kernel-stable.pacscript"
   fi
-  echo
+
+  sed -i "".bak "s/pkgver=\".*\"/pkgver=\"$pkgver\"/" "$kernel_script"
+  rm packages/linux-kernel*/*.bak
+}
+
+unset stable input_data gitker_remote
+promptless=false
+newbranch=false
+
+while (($# > 0)); do
+  key="$1"
+  case $key in
+    -S | -s | --stable)
+      stable=true
+      shift
+      ;;
+    -M | -m | --mainline)
+      stable=false
+      shift
+      ;;
+    -V | -v | --version)
+      kerver="${2/\~/-}"
+      fetch_kver
+      shift
+      shift
+      ;;
+    -Y | -y | --yes)
+      promptless=true
+      shift
+      ;;
+    -B | -b | --branch)
+      newbranch=true
+      shift
+      ;;
+    -P | -p | --push)
+      gitker_remote="$2"
+      shift
+      shift
+      ;;
+  esac
 done
 
-kernel_script="packages/linux-kernel/linux-kernel.pacscript"
-[[ $stable == "y" || $stable == "Y" ]] && kernel_script="packages/linux-kernel-stable/linux-kernel-stable.pacscript"
+unset kerselected
+if [[ -z ${stable} && -n ${kerver} ]]; then
+  if [[ ${kerver} == *"rc"* ]]; then
+    echo "${BGreen}INFO${NC}: selecting ${BPurple}mainline${NC}."
+    kerselected="main"
+    stable=false
+    update_ker
+  elif [[ $kerver =~ \.0$ ]] || ! [[ $kerver =~ \.[0-9]+\.[0-9]+$ ]]; then
+    echo "${BGreen}INFO${NC}: selecting both ${BPurple}mainline${NC} and ${BPurple}stable${NC}."
+    kerselected="both"
+    stable=true
+    update_ker && \
+    stable=false
+    update_ker
+  else
+    echo "${BGreen}INFO${NC}: selecting ${BPurple}stable${NC}."
+    kerselected="stab"
+    stable=true
+    update_ker
+  fi
+else
+  if ${stable}; then
+    kerselected="stab"
+  else
+    kerselected="main"
+  fi
+  update_ker
+fi
 
-sed -i "".bak "s/pkgver=\".*\"/pkgver=\"$pkgver\"/" "$kernel_script"
-rm packages/*/*bak
+unset gitker_input
+case ${kerselected} in
+  both) gitker_input="linux-kernel{-stable}" ;;
+  main) gitker_input="linux-kernel" ;;
+  stab) gitker_input="linux-kernel-stable" ;;
+esac
 
-kerstable=""
-[[ $stable == "y" || $stable == "Y" ]] && kerstable="-stable"
-
-git add packages/*/*pacscript
-git commit -m "upd(linux-kernel${kerstable}): \`$original_old_pkgver\` -> \`$pkgver\`"
+git add packages/linux*/*pacscript
+git commit -m "upd(${gitker_input}): \`$original_old_pkgver\` -> \`$pkgver\`" && \
+if ${newbranch} && [[ -n ${gitker_remote} ]]; then
+  git push ${gitker_remote} HEAD:${gitker_branch}
+fi
